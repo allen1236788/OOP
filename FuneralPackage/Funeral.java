5c9920c623191df1f71bff4b0cb8a795dce1b198
Xalleniee <charllencalvo@gmail.com>
2025-03-19 22:17:04 -0500

    new fp

--- FuneralPackage/Funeral.java
+++ FuneralPackage/Funeral.java
@@ -4,16 +4,40 @@ import java.util.Scanner;
 public class Funeral {
     public static void main(String[] args) {
         ArrayList<FuneralService>services=new ArrayList<>();
-        String nem;
+        String nem,whatt,date,fd,cementary;
+        double costt;
+        boolean urn;
+       
         Scanner S= new Scanner(System.in);
         System.out.print("Enter client Name: ");
         nem=S.nextLine();
+        System.out.print("Enter Service Type: ");
+        whatt=S.nextLine();
+        System.out.print("Enter Date of service: ");
+        date=S.nextLine();
+        System.out.print("Enter Funeral director: ");
+        fd=S.nextLine();
 
+        if(whatt.equals("Cremation")){
+            System.out.print("Urn Included? true/false: ");
+            urn=S.nextBoolean();
+            services.add(new CremationService(nem,whatt,date, 1500.00,fd, urn));
+            services.add(new CremationService(nem,whatt,date, 100.00,fd, urn));
 
-        services.add(new CremationService(nem,"Cremation","2025-03-25", 1500.00,"Alice Verja", true));
-        services.add(new BurialService("Jane smith","Burial", "2025-03-26", 3000.00, "Bob Lucas", "Bacolod Public cementary"));
-        services.add(new CremationService("Mark White", "Cremation","2025-03-27",1300.00,"Sally Monte", false));
-
+          /*   for(FuneralService service:services){
+                if(service.getServiceType().equals("Cremation")){
+                    System.out.println("This is a cremation service. Details:");
+                }else if (service.getServiceType().equals("Burial")){
+                    System.out.println("This is a burial service. Details:");
+                }
+                service.displayServiceDetails();
+                System.out.println();
+            } */
+        }else{
+            System.out.println("Enter Cementary name: ");
+            cementary=S.nextLine();
+        services.add(new BurialService(nem,whatt,date, 3000.00,fd,cementary));
+        }
         for(FuneralService service:services){
             if(service.getServiceType().equals("Cremation")){
                 System.out.println("This is a cremation service. Details:");
@@ -23,5 +47,6 @@ public class Funeral {
             service.displayServiceDetails();
             System.out.println();
         }
+        
     }
 }
